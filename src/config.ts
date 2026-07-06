0c795dcfc9e9f42bc69c503eae58119e16b9698f
Loyal Star <luoyal@yeah.net>
2026-07-06 15:14:59 +0800

    update：
    src/config.ts:4：SITE_TITLE 改成你的名字或站点名，例如 "张三的个人主页"
    src/config.ts:5：AUTHOR_NAME 改成作者名，例如 "张三"
    src/config.ts:6：AUTHOR_INITIAL 改成标题前缀，例如 "ZS" 或 "张"
    src/config.ts:7：SITE_DESCRIPTION 改成中文站点描述

--- src/config.ts
+++ src/config.ts
@@ -1,10 +1,10 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Your Name";
-export const AUTHOR_NAME = "Your Name";
-export const AUTHOR_INITIAL = "YN";
-export const SITE_DESCRIPTION = "Your Name's personal website.";
+export const SITE_TITLE = "有饭有娃有日子";
+export const AUTHOR_NAME = "Loyal";
+export const AUTHOR_INITIAL = "YL";
+export const SITE_DESCRIPTION = "记录自然生活的日子";
 export const GENERATE_SLUG_FROM_TITLE = true;
 export const TRANSITION_API = true;
 
